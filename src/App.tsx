8053e2df32b7c2602584ad842cb66c6a5269af7d
Angie-07 <angiecb1707@gmail.com>
2023-11-30 17:11:40 -0500

    feat: changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home";
 import { ThemeProvider } from "styled-components";
@@ -10,22 +8,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <Home />
     </ThemeProvider>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
